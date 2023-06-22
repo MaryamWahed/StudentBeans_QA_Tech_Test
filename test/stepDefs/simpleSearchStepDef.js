dacef6383053564d1b96260c1777f942a5669ed7
maryam <maryam-19@live.nl>
2023-06-22 22:48:15 +0100

    add simple search feature

--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -6,4 +6,17 @@ const simpleSearchPageObject = new SimpleSearchPageObject()
 Given('I am on the studentbeans homepage', async () => {
   await simpleSearchPageObject.goToHomePage()
   await simpleSearchPageObject.verifyHomePage()
-})
+});
+
+Given('I open the search bar', async () => {
+  await simpleSearchPageObject.openTheSearchBar()
+ });
+ 
+When('I enter {string}', async (searchTerm) => {
+   await simpleSearchPageObject.searchTheTermInSearchBar(searchTerm)
+  });
+ 
+Then('I should select the 4th {string} search listing', async (searchTerm) => {
+   await simpleSearchPageObject.selectTheSearchTerm()
+   await simpleSearchPageObject.verifySearchTerm(searchTerm)
+  });
